b721e88ea21490efbcc7c81e3c300815f9b7a5ec
icarodavi <icarodavi@gmail.com>
2018-04-18 13:48:34 -0300

    Desafio 01

--- d1/index.js
+++ d1/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
 app.listen(3000);
