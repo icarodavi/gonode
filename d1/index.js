02e7365b8f2ab7d31c2a498823134c804af35998
icarodavi <icarodavi@gmail.com>
2018-05-07 18:03:07 -0300

    Desafio 01

--- d1/index.js
+++ d1/index.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-var express = require('express');
-var nunjucks = require('nunjucks');
-
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
 const app = express();
 
 nunjucks.configure({
@@ -10,30 +9,14 @@ nunjucks.configure({
 });
 
 app.set('view engine', 'njk');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.redirect('/main');
+});
+
+app.get('/main', (req, res) => {
+  res.render('index');
 });
 
 app.listen(3000);
-=======
-var express = require('express');
-var nunjucks = require('nunjucks');
-var path = require('path');
-
-const app = express();
-
-nunjucks.configure({
-  autoescape: true,
-  express: app,
-});
-
-app.set('view engine', 'njk');
-app.set('views', path.join(__dirname,'views'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.listen(3000);
->>>>>>> 583b702ba6e925cb4b493f7d03d0c67654d969b0
